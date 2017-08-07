a3da058649850d82ced3a77c8426560bf3f1ac8c
Lauri Keisanen <lauri.keisanen@aalto.fi>
2017-08-07 15:27:02 +0300

    Start https server if in production
    Http server is convenient when developing on localhost and there are no ssl
    certificates, but when running the API on a server we want https.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./queries');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
@@ -20,6 +24,16 @@ app.get('/robots.txt', (req, res) => {
 
 app.post('/api/guess', db.saveGuess);
 
-app.listen(port, () => {
-    console.log(`API is running on port ${port}`);
-});
+if (process.env.NODE_ENV === 'production') {
+    const options = {
+        key: fs.readFileSync('path/to/keys/placeholder-key.pem'),
+        cert: fs.readFileSync('path/to/keys/placeholder-cert.pem'),
+    };
+    https.createServer(options, app).listen(port, () => {
+        console.log(`API is running on port ${port}`);
+    });
+} else {
+    http.createServer(app).listen(port, () => {
+        console.log(`API is running in dev mode on port ${port}`);
+    });
+}
